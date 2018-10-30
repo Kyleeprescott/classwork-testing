8a48a33b22e5e1643ffbd51f597bc8950ad4a7f4
kylee.prescott <kylee.prescott@CLC-LAB209-09.TCSD.tooelesd.org>
2018-10-30 13:05:13 -0600

    classwork

---
+++ src/testjava/testjava.java
@@ -0,0 +1,51 @@
+package testjava;
+
+public class testjava {
+
+	public static void main(String[] args) {
+		char char1 ='\u0030' ;
+		char char2 ='\u0031' ;
+		char char3 ='\u0032' ;
+		char char4 ='\u0033' ;
+		char char5 ='\u0034' ;
+		char char6 ='\u0035' ;
+		char char7 ='\u0036' ;
+		char char8 ='\u0037' ;
+		char char9 ='\u0038' ;
+		char char10 ='\u0039' ;
+		
+		System.out.println( char1+""+char2+""+char3+""+char4+""+char5+""+ char6+""+ char7+""+ char8+""+ char9+
+				""+ char10+"");
+		char A ='\u0041' ;
+		char B ='\u0042' ;
+		char C ='\u0043' ;
+		char D ='\u0044' ;
+		char E ='\u0044' ;
+		char F ='\u0045' ;
+		char G ='\u0046' ;
+		char H ='\u0047' ;
+		char I ='\u0048' ;
+	    char J ='\u0049' ;
+		char K ='\u004A' ;
+		char L ='\u004B' ;
+		char M ='\u004C' ;
+		char N ='\u004D' ;
+		char O ='\u004F' ;
+		char P ='\u0050' ;
+		char Q ='\u0051' ;
+		char R ='\u0052' ;
+		char S ='\u0053' ;
+		char T ='\u0054' ;
+		char U ='\u0055' ;
+		char V ='\u0056' ;
+		char W ='\u0057' ;
+		char X ='\u0058' ;
+		char Y ='\u0059' ;
+		char Z ='\u005A' ;
+	
+		
+		
+
+	}
+
+}
