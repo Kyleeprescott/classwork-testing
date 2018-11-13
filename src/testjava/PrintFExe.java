8f4bf94d1b57c8fe63466a5461d32738c839c2be
kylee.prescott <kylee.prescott@CLC-LAB209-09.TCSD.tooelesd.org>
2018-11-13 14:11:25 -0700

    work

---
+++ src/testjava/PrintFExe.java
@@ -0,0 +1,21 @@
+package testjava;
+
+public class PrintFExe {
+
+	public static void main(String[] args) {
+		//double amount = 12618.98;
+		//double interestRate = .0013;
+		//double interest = amount*interestRate;
+		//System.out.printf( "Interest is $%4.2f", interest);
+		
+		//int count = 5;
+		//double amount = 45.56;
+		//System.out.printf("count is %d and amount is %f",count , amount);
+		
+	//String word = "hello world";
+	//System.out.printf("%s", word);
+		System.out.printf("%")
+
+	}
+
+}
